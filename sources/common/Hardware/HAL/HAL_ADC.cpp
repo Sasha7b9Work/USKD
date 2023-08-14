cca561ab4c935f8a373b4964e57d1c3619c62003
Sasha7b9Work <Phuntik-it@tut.by>
2023-08-14 14:18:02 +0300

    *

--- sources/common/Hardware/HAL/HAL_ADC.cpp
+++ sources/common/Hardware/HAL/HAL_ADC.cpp
@@ -8,6 +8,12 @@
 namespace HAL_ADC
 {
     static float voltage = 0.0f;
+
+    static float ConversionRawToVoltageADC(uint);
+
+    static float ConversionVoltageADCtoVoltageBattery(float);
+
+    static float ConversionRawToVoltageBattery(uint);
 }
 
 
@@ -15,32 +21,54 @@ void HAL_ADC::Init()
 {
     pinBAT.Init();
 
-
-    adc_mode_config(ADC_MODE_FREE);
+    adc_special_function_config(ADC0, ADC_CONTINUOUS_MODE, ENABLE);
 
     adc_data_alignment_config(ADC0, ADC_DATAALIGN_RIGHT);
 
-    adc_special_function_config(ADC0, ADC_SCAN_MODE, ENABLE);
+    adc_channel_length_config(ADC0, ADC_REGULAR_CHANNEL, 2);
 
-    adc_channel_length_config(ADC0, ADC_INSERTED_CHANNEL, 2);
-
-    adc_inserted_channel_config(ADC0, 0, ADC_CHANNEL_1, ADC_SAMPLETIME_239POINT5);
-
-    adc_inserted_channel_config(ADC0, 1, ADC_CHANNEL_17, ADC_SAMPLETIME_239POINT5);
-
-    adc_external_trigger_config(ADC0, ADC_INSERTED_CHANNEL, ENABLE);
-
-    adc_external_trigger_source_config(ADC0, ADC_INSERTED_CHANNEL, ADC0_1_2_EXTTRIG_INSERTED_NONE);
+    adc_regular_channel_config(ADC0, 0, ADC_CHANNEL_1, ADC_SAMPLETIME_239POINT5);
 
     adc_enable(ADC0);
 
+    adc_software_trigger_enable(ADC0, ADC_CHANNEL_1);
+
     Timer::DelayMS(1);
 
-    adc_calibration_enable(ADC0);
+    adc_calibration_enable(ADC1);
 }
 
 
 float HAL_ADC::GetVoltage()
 {
+    if (adc_flag_get(ADC0, ADC_FLAG_EOC) == SET)
+    {
+        voltage = ConversionRawToVoltageBattery(ADC_RDATA(ADC0));
+
+        adc_flag_clear(ADC0, ADC_FLAG_EOC);
+    }
+
     return voltage;
+}
+
+
+float HAL_ADC::ConversionRawToVoltageADC(uint raw)
+{
+    static const float MAX_VOLTAGE = 3.0f;
+
+    return MAX_VOLTAGE / (1 << 12) * (float)raw;
+}
+
+
+float HAL_ADC::ConversionVoltageADCtoVoltageBattery(float adc)
+{
+    return -0.6f * adc + 4.36f;
+}
+
+
+float HAL_ADC::ConversionRawToVoltageBattery(uint raw)
+{
+    float voltageADC = ConversionRawToVoltageADC(raw);
+
+    return ConversionVoltageADCtoVoltageBattery(voltageADC);
 }
